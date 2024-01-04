f8ec46becb842c5dd07084bdd4c73f436d086b8d
mehrzad <merzaad@gmail.com>
2024-01-04 09:08:38 +0330

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import http from "http";
 import predict from "./predict.js";
 
-const port = 3000;
+const port = 6666;
+const hostname = "127.0.0.1";
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
   const searchParams = new URLSearchParams(req.url.split("/")[1]);
   const number = searchParams.get("number");
-  console.log("confi");
   if (number) {
     const predicted = await predict(Number(number));
     res.statusCode = 200;
@@ -17,8 +17,8 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 400;
     res.end('{"error":"Invalid number"}');
   }
-  console.log("add");
+  console.log("conflict");
 });
 server.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 });
